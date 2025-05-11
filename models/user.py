14bae721b7eeac08403068d1145ad9bbfa0d1bc7
c-hirwa <c.hirwa@alustudent.com>
2025-05-11 23:48:42 +0200

    Create user.py

---
+++ models/user.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""User model class"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """A class for the User model"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initialize the data model"""
+        super().__init__(*args, **kwargs)
