d0603f4f3a38674eeaa449a5829a6028f1a45626
c-hirwa <c.hirwa@alustudent.com>
2025-05-11 23:50:55 +0200

    Create test_state.py

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" """
+from tests.test_models.test_base_model import test_basemodel
+from models.state import State
+
+
+class test_state(test_basemodel):
+    """ model state """
+
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
+        self.name = "State"
+        self.value = State
+
+    def test_name3(self):
+        """ The name should be a string """
+        new = self.value()
+        self.assertEqual(type(new.name), str)
